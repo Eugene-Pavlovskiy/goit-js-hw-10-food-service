1296573341e55d5065f7d43eb7a279c199dfb29e
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-08-30 20:54:32 +0300

    added toolbar switching theme

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,42 @@
-import menuData from '../menu.json';
-import menuCardTemplate from '../templates/menu.hbs';
+import menuTemplate from '../templates/menu.hbs';
+import menu from './menu.json';
 
-const menu = document.querySelector('.js-menu');
+const menuMarkup = document.querySelector('.js-menu');
+const cardMarkup = createMenuMarkup(menu);
+menuMarkup.insertAdjacentHTML('beforeend', cardMarkup );
+function createMenuMarkup(menu) {
+    return menu.map(menuTemplate).join("");
+}
+const body = document.querySelector('body');
+const toolbox = document.querySelector("#theme-switch-toggle");
 
-const menuCardMarkup = menuCardTemplate(menuData);
-menu.insertAdjacentHTML('beforeend', menuCardMarkup);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+
+function setThemeName(themeName) {
+    localStorage.setItem('theme', themeName);
+    body.className = themeName;
+}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === LIGHT) {
+        setThemeName(DARK);
+    } else {
+        setThemeName(LIGHT);
+    }
+}
+function saveTheme() {
+    if (localStorage.getItem('theme') === DARK) {
+        setThemeName(DARK);
+        toolbox.checked = true;
+    } else {
+        setThemeName(LIGHT);
+        toolbox.checked = false;
+    }
+};
+
+saveTheme();
+toolbox.addEventListener('change', toggleTheme); 
